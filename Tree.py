7f7763b2d95c36467ea740df94758d0268f07760
Gabriel <38161549+gabrielwere@users.noreply.github.com>
2021-04-07 23:25:37 +0300

    Add files via upload

---
+++ Tree.py
@@ -0,0 +1,31 @@
+class TreeNode:
+    def __init__(self,data):
+        self.data = data
+        self.children = []
+        self.parent = None
+
+    def __repr__(self, level=0):
+        ret = "    "*level+repr(self.data)+"\n"
+        for child in self.children:
+            ret += child.__repr__(level+1)
+        return ret
+
+    def addChild(self,child):
+        child.parent = self
+        self.children.append(child)
+
+        #were
+    
+    def removeChild(self,child):
+        child.parent = None
+        self.children.remove(child)
+
+
+
+
+
+
+    
+
+    
+
